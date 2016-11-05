f846917f4c3badb67dd4592be68c7b383aa006c0
Rudiger Roach <rudigerroach@gmail.com>
2016-11-05 11:05:40 +0200

    add breadcrumbs as a seperate component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,86 +1,93 @@
 //external module
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
-import { AlertModule, DatepickerModule } from 'ng2-bootstrap/ng2-bootstrap';
-import { AngularFireModule, FirebaseAppConfig } from 'angularfire2';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {RouterModule} from '@angular/router';
+import {AlertModule, DatepickerModule} from 'ng2-bootstrap/ng2-bootstrap';
+import {AngularFireModule, FirebaseAppConfig} from 'angularfire2';
 import {ToasterModule} from 'angular2-toaster/angular2-toaster';
 
 export const firebaseConfig: FirebaseAppConfig = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  storageBucket: ''
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    storageBucket: ''
 };
 
 let modules = [
-  AlertModule,
-  DatepickerModule,
-  BrowserModule,
-  FormsModule,
-  HttpModule,
-  RouterModule,
-  AngularFireModule.initializeApp(firebaseConfig),
-  ToasterModule
+    AlertModule,
+    DatepickerModule,
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    ToasterModule
 ];
 
-import { AppHeaderComponent } from "./widgets/app-header";
-import { MenuAsideComponent } from "./widgets/menu-aside";
-import { MessagesBoxComponent} from "./widgets/messages-box";
-import { NotificationBoxComponent } from "./widgets/notification-box";
-import { TasksBoxComponent } from "./widgets/tasks-box";
-import { UserBoxComponent } from "./widgets/user-box"
+import {AppHeaderComponent} from "./widgets/app-header";
+import {MenuAsideComponent} from "./widgets/menu-aside";
+import {MessagesBoxComponent} from "./widgets/messages-box";
+import {NotificationBoxComponent} from "./widgets/notification-box";
+import {TasksBoxComponent} from "./widgets/tasks-box";
+import {UserBoxComponent} from "./widgets/user-box"
 
 let widgets = [
-  AppComponent,
-  AppHeaderComponent,
-  MenuAsideComponent,
-  MessagesBoxComponent,
-  NotificationBoxComponent,
-  TasksBoxComponent,
-  UserBoxComponent
+    AppComponent,
+    AppHeaderComponent,
+    MenuAsideComponent,
+    MessagesBoxComponent,
+    NotificationBoxComponent,
+    TasksBoxComponent,
+    UserBoxComponent
 ];
 
-import { UserService } from "./services/user.service";
-import { MessagesService } from "./services/messages.service";
+import {UserService} from "./services/user.service";
+import {MessagesService} from "./services/messages.service";
 
-let services =  [
-  UserService,
-  MessagesService
+let services = [
+    UserService,
+    MessagesService
 ];
 
-import { HomeComponent } from './pages/home/home.component';
-import { PageNumComponent } from './pages/page-num/page-num.component';
-import { ClientComponent } from './pages/client/client.component';
+import {HomeComponent} from './pages/home/home.component';
+import {PageNumComponent} from './pages/page-num/page-num.component';
+import {ClientComponent} from './pages/client/client.component';
+import {BreacrumbsComponent} from './pages/fragments/breadcrumbs/breadcrumbs.component';
 
 let pages = [
-  HomeComponent,
-  PageNumComponent,
-  ClientComponent,
-  DashboardComponent,
-  CalendarComponent
-]
+    HomeComponent,
+    PageNumComponent,
+    ClientComponent,
+    DashboardComponent,
+    CalendarComponent
+];
+
+let pageFragments = [
+    BreacrumbsComponent
+];
 
 //main bootstrap
-import { AppComponent } from './app.component';
-import { routing } from './app.routes';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { CalendarComponent } from './pages/calendar/calendar.component';
+import {AppComponent} from './app.component';
+import {routing} from './app.routes';
+import {DashboardComponent} from './pages/dashboard/dashboard.component';
+import {CalendarComponent} from './pages/calendar/calendar.component';
 
 @NgModule({
-  declarations: [
-    ...widgets,
-    ...pages,
-  ],
-  imports: [
-    ...modules,
-    routing
-  ],
-  providers: [
-    ...services
-  ],
-  bootstrap: [AppComponent]
+    declarations: [
+        ...widgets,
+        ...pages,
+        ...pageFragments
+    ],
+    imports: [
+        ...modules,
+        routing
+    ],
+    providers: [
+        ...services
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {PageBreadCrumb} from '../fragments/breadcrumbs/PageBreadCrumb';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  breadcrumbs: PageBreadCrumb;
 
   constructor() { }
 
   ngOnInit() {
+    this.breadcrumbs = <PageBreadCrumb>{};
+    this.breadcrumbs.header = "Dashboard";
+    this.breadcrumbs.subHeader ="An overview of the system";
+    this.breadcrumbs.iconName = "fa-dashboard";
+    this.breadcrumbs.trail = ["Dashboard"];
   }
 
 }
